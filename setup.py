d8a996754e6f66b906370bfb5c928b7ee3da91cd
Sebastian Bordt <sbordt@posteo.de>
2022-10-20 12:20:14 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nshap",
+    version="0.0.1",
+    author="Sebastian Bordt",
+    author_email="sbordt@posteo.de",
+    description="Python package to compute n-Shapley Values.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tml-tuebingen/nshap",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "seaborn",
+    ],
+)
